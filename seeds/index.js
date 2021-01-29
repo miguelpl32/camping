62f7a3d01c3e5ee8c0966979f4b22b9a728620d1
Miguel-Pariente <miguelpl32@hotmail.com>
2021-01-29 09:15:24 +0100

    creacion estilo basico y rutas

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,14 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/483251",
+            description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita facere eum quia perferendis neque a fuga, officia aut amet nam officiis, voluptatem modi temporibus tempore?",
+            price,
         });
         await camp.save();
     }
