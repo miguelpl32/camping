6fa9c8782ae566f05bd5bdd94c72dd2c48ec2b01
Miguel-Pariente <miguelpl32@hotmail.com>
2021-02-07 11:14:59 +0100

    relacion mogo, estilo, eliminacion reviews

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const CampGroundSchema = new Schema({
@@ -7,6 +8,24 @@ const CampGroundSchema = new Schema({
     price: Number,
     description: String,
     location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
+
+// Eliminar todos los reviews si se elimina el campamento.
+
+CampGroundSchema.post('findOneAndDelete', async function(doc) {
+    if(doc){
+        await Review.deleteMany({
+            _id:{
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model("Campground", CampGroundSchema);
