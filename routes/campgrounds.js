9cf29c7624091bf85ce369573db70b0985439167
Miguel-Pariente <miguelpl32@hotmail.com>
2021-02-19 09:13:08 +0100

    Upload imagenes, delete

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,20 +3,26 @@ const router = express.Router();
 const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
+const multer = require('multer');
+const {storage} = require('../cloudinary')
+const upload = multer({ storage})
 
 // RUTAS CAMPGROUNDS
 router.get("/", catchAsync(campgrounds.index));
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
-router.post("/", isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+router.post("/", isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
+
+
+
 
 router.get("/:id", catchAsync(campgrounds.showCampground));
 
 router.get("/:id/edit", isLoggedIn,isAuthor,catchAsync(campgrounds.renderEditForm));
 
 
-router.put("/:id",isLoggedIn,isAuthor,validateCampground,catchAsync(campgrounds.updateCampground));
+router.put("/:id",isLoggedIn,isAuthor, upload.array('image'), validateCampground,catchAsync(campgrounds.updateCampground));
 
 router.delete("/:id",isLoggedIn,isAuthor,catchAsync(campgrounds.deleteCampground));
 
