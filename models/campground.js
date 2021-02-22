8e9bcc911772cb642acc6f89c334ce3b35ae01b0
Miguel-Pariente <miguelpl32@hotmail.com>
2021-02-22 13:40:51 +0100

    Creación Mapa principal

--- models/campground.js
+++ models/campground.js
@@ -13,10 +13,23 @@ ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200');
 });
 
+const opts = { toJSON: { virtuals: true } };
+
 
 const CampGroundSchema = new Schema({
     title: String,
     images: [ImageSchema],
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type:[Number],
+            required: true
+        }
+    },
     price: Number,
     description: String,
     location: String,
@@ -30,6 +43,13 @@ const CampGroundSchema = new Schema({
             ref: "Review",
         },
     ],
+   
+}, opts);
+
+CampGroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+        <strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+        <p>${this.description.substring(0,30)}...</p>`;
 });
 
 // Eliminar todos los reviews si se elimina el campamento.
@@ -43,5 +63,8 @@ CampGroundSchema.post("findOneAndDelete", async function (doc) {
         });
     }
 });
+
+
+
 
 module.exports = mongoose.model("Campground", CampGroundSchema);
