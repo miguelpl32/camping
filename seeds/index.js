4c03014ff52b00e0ea279214c4c74f7f481e7b8d
Miguel-Pariente <miguelpl32@hotmail.com>
2021-02-23 19:01:58 +0100

    Modificacion conexion

--- seeds/index.js
+++ seeds/index.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
+require ('dotenv'). config ();
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+const dbUrl = process.env.DB_URL 
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
