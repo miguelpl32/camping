ba2031f92962575d19cfde3229df6c0f34c0c7e7
Miguel-Pariente <miguelpl32@hotmail.com>
2021-02-23 12:35:54 +0100

    modificacion script app

--- app.js
+++ app.js
@@ -168,6 +168,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
-    console.log("Conectado en el Puerto 3000");
+const port = process.env.PORT || 3000;
+app.listen( port, () => {
+    console.log(`Conectado en el Puerto: ${port}`);
 });
